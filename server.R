4c2a37792888599b9222d58fa6f42904e9bee7a4
Yuzhi <yli276@ncsu.edu>
2022-12-12 02:44:12 -0500

    First version of App

--- server.R
+++ server.R
@@ -1,14 +1,120 @@
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(caret)
+library(rsample)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
 
-
+diabetes<- read.csv("diabetes.csv")
+diabetes$Outcome <- as.factor(diabetes$Outcome)
 
 server <- function(input, output, session){
   
-  diabetes<- read.csv("diabetes.csv")
-  
-  output$plot1<- renderPlot({
-    hist<-hist(diabetes$Glucose)
+  #Create reactive dataframe
+  diabetes_new <- reactive({
+    diabetes[input$rows[1]:input$rows[2],input$variable]
   })
+  ## 
+  output$plot1<- renderPlot({
+
+    hist(diabetes_new(),xlab = input$variable)
+
+  })
+  output$plot2<- renderPlot({
+    d<- density(diabetes_new())
+    plot(d, main = "Kernel Density", xlab = input$variable)
+  })
+  output$summary<- renderPrint({
+    summary(diabetes_new())
+  }) 
+  output$ex1<- renderUI({
+    withMathJax(helpText('The general equation is $$\\log(P/1-P) = \\beta_{null} + \\beta_1X_1 + ... + \\beta_nX_n$$'))
+  })
+  
+  # split data to train and test set and reactive the data set
+  
+  diabetes$id<- c(1:nrow(diabetes))
+  train_set <- reactive({
+    index<- initial_split(diabetes, prop = input$train)
+    training(index)
+  }) 
+  
+  test_set<- reactive({
+    diabetes %>% anti_join(train_set(), by=c("id"))
+  })
+  
+  ## Logistic regression model
+  mod1<- reactive({
+    train_control<- trainControl(method = "cv", number =10)
+    logit<- train(Outcome ~., 
+                  data=train_set()[,-10],
+                  trControl = train_control,
+                  method = "glm",
+                  family = binomial())
+  })
+    output$logit_sum<- renderPrint({
+      print(summary(mod1()))
+      })
+    
+    ## Accuracy on training data
+    output$logit_train_pro<- renderPrint({
+      pred_train<- predict(mod1(), newdata=train_set()[,-10], type = "prob")
+      pred_train$var<- as.numeric(pred_train[,1]<pred_train[,2])
+      pred_train$var<- as.factor(pred_train$var)
+      cm<- confusionMatrix(data=pred_train$var, reference = train_set()$Outcome)
+      print(cm)
+    })
+    
+    ## Accuracy on testing data
+    output$logit_test_pro<- renderPrint({
+      pred_test<- predict(mod1(), newdata=test_set()[,-10], type = "prob")
+      pred_test$var<- as.numeric(pred_test[,1]<pred_test[,2])
+      pred_test$var<- as.factor(pred_test$var)
+      cm_t<- confusionMatrix(data=pred_test$var, reference = test_set()$Outcome)
+      print(cm_t)
+    })
+  
+    ## Classification tree model
+    mod2<- reactive({
+      class_tree<- rpart(Outcome ~., data= train_set()[,-10], method = 'class')
+    })
+    output$class_plot<- renderPlot({
+      rpart.plot(mod2(), extra = 106)
+    })
+    output$pred_train <- renderPrint({
+      pred_train<- predict(mod2(), train_set()[,-10], type = "class")
+      table(train_set()$Outcome, pred_train)
+    })
+    output$pred_test <- renderPrint({
+      pred_test<- predict(mod2(), test_set()[,-10], type = "class")
+      table(test_set()$Outcome, pred_test)
+    })
+
+    ## Random forest model
+    mod3<- reactive({
+      ran_forest<- randomForest(Outcome ~., data=train_set()[,-10], 
+                                mtry = ncol(train_set()[,-10])/3,
+                                ntree = 100, importance = TRUE)
+    })
+    output$rf_sum <- renderPrint({
+      print(mod3())
+    })
+    output$rf_test<- renderPrint({
+      pred_test<- predict(mod3(), test_set())
+      confusionMatrix(pred_test,test_set()$Outcome)
+    })
+    new_data<- reactive({
+      data.frame(Pregnancies=input$Preg, Glucose=input$Glu, BloodPressure=input$BP, SkinThickness=input$ST, Insulin=input$Ins, BMI=input$BMI, DiabetesPedigreeFunction=input$DPF, Age=input$Age)
+    })
+    output$new_pred<- renderPrint({
+      pred<- predict(mod1(), newdata=new_data(), type = "prob")
+      print(pred)
+    })
+    
+    diabetes_re<- reactive({diabetes[,-10]})
+    
 }
+
 
--- ui.R
+++ ui.R
@@ -1,5 +1,11 @@
 library(shinydashboard)
 library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(caret)
+library(rsample)
+library(rpart)
+library(rpart.plot)
 
 ui<- dashboardPage(
   dashboardHeader(title = "Diabetes Prediction"),
@@ -7,7 +13,11 @@ ui<- dashboardPage(
     sidebarMenu(
       menuItem("About", tabName = "About"),
       menuItem("Exploration", tabName = "Exploration"),
-      menuItem("Model", tabName = "Model"),
+      menuItem("Modeling", tabName = "Modeling",
+               startExpended = TRUE,
+               menuSubItem("Modeling Info", tabName = "MInfo"),
+               menuSubItem("Model Fitting", tabName = "MFitting"),
+               menuSubItem("Prediction", tabName = "Prediction")),
       menuItem("Data", tabName = "Data")
     )
   ),
@@ -28,20 +38,71 @@ ui<- dashboardPage(
     
     # Second tab content
     tabItem(tabName = "Exploration",
+            
+              selectInput("summary", "Summary Type", c("Histogram plot", "Density plot", "Numeric summary")),
+              selectInput("variable", "Select Variable", choices=colnames(diabetes[,-9])),
+              sliderInput("rows", "Number of Rows", min = 1, max = 768, step = 50, value = c(1,768)
+            ),
             fluidRow(
-              box(plotOutput ("plot1", height = 200)),
-              
-              box(
-                selectInput( "plotType", "Plot Type",
-                            c(Histogram = "hist"))
-              )
+             conditionalPanel(
+               condition = "input.summary == 'Histogram plot'",
+                box(title = "Histogram plot", plotOutput ("plot1",                                            height = 350))),
+             conditionalPanel(
+               condition = "input.summary == 'Density plot'",
+               box(tittle = "Density plot", plotOutput ("plot2"), height = 350)
+            ),
+            conditionalPanel(
+              condition = "input.summary == 'Numeric summary'",
+              box(title = "Numeric summary", verbatimTextOutput("summary"))
+            )
             )),
     
     # Third tab content
-    tabItem(tabName = "Model"),
+            tabItem(tabName = "MInfo",
+                    fluidRow(
+                      column(12,
+                             span(h4("1. Generalized linear regression allow us to build a linear relationship between the response and predictors when their relationship is not linear. In this case, the response variable is categorical. However, this model is not for data which are correlated with each other."))),
+              uiOutput("ex1"),
+              column(12,
+                     span(h4("2. Classification tree is to analyze and predict the class via tree-building algorithms. Some of the advantages are easy to interpret and visualize, less effort for pre-processing of data and no need to make assumptions about classifier structure. However, overftting can be one of problems. A small change in the data trends might cause a big difference in the tree structure."))),
+              column(12,
+                     span(h4("3. Random forest is a supervised machine learning algorith that is used widely in classification. Random Forests uses the same idea as bagging but only use a random subset of predictors for each booststrap. It will then average the results of the multiple trees created. Some of the advantages are no need to scale or transform the variables, handling well for both linear and non-linear reatltionships and balance the bias-variance trade-off. However, random forests are not easy to interpret and might be computationally intensive for large data sets.")))
+                    )),
+           tabItem(tabName = "MFitting",
+                   sliderInput("train", "Select proportion for training data set:", min = 0.1, max = 0.9, step = 0.1, value = 0.1),
+                   selectInput("model", "Select the model:", c("Logistic regression", "Classification tree", "Random forest")),
+                   actionButton("button", "Click to fit the model"),
+              conditionalPanel(condition = "input.model == 'Logistic regression'",     
+                   box(title = "Model summaries", verbatimTextOutput("logit_sum")),
+                   box(title = "Train data prediction", verbatimTextOutput(("logit_train_pro"))),
+                   box(title = "Test data prediction", verbatimTextOutput("logit_test_pro"))),
+              conditionalPanel(condition = "input.model == 'Classification tree'",
+                               box(title = "Classification tree fit model", plotOutput("class_plot")),
+                               box(title = "Prediction on training data", verbatimTextOutput("pred_train")),
+                               box(title = "Prediction on testing data", verbatimTextOutput("pred_test"))),
+              conditionalPanel(condition = "input.model == 'Random forest'",
+                              box(title = "Model summaries", verbatimTextOutput("rf_sum")),
+                              box(title = "Prediction accuracy on testing data", verbatimTextOutput(("rf_test"))))
+                   ),
+           
+    tabItem(tabName = "Prediction",
+            sliderInput("Preg", "Pregnancies", min= 0, max = 10, step = 1, value = 1),
+            sliderInput("Glu", "Glucose", min=50, max = 200, step = 10, value = 90),
+            sliderInput("BP", "BloodPressure", min=30, max=120, step =10, value = 80),
+            sliderInput("ST","SkinThickness", min=0, max=100, step = 10, value = 20),
+            sliderInput("Ins","Insulin", min=0, max=200, step=10, value=50),
+            sliderInput("BMI","BMI", min=15, max =50, step=5, value=30),
+            sliderInput("DPF","DiabetesPedigreeFunction", min=0, max=5, step=0.5, value=1),
+            sliderInput("Age","Age",min=0, max=100, step=5, value=20),
+            box(title ="Predction of probability of having diabetes", verbatimTextOutput("new_pred"))),
+            
     
     #Fourth tab content
-    tabItem("Data")
-  )
-)
-)
+    tabItem(tabName = "Data",
+         
+              
+            )
+  
+)))
+
+
